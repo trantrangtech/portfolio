231ba72739bc7dc0dd9134966d443ddd5881a8af
trantrangtech <trantrang.tech@gmail.com>
2025-01-06 15:41:20 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
+    output: 'export', // Thêm dòng này
+    basePath: '/portfolio', // Thêm tên repository của bạn
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
