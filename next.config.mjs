fee09edf52ac40d2c3e9515fd6e7149714ad381a
trantrangtech <trantrang.tech@gmail.com>
2025-01-06 14:09:39 +0700

    Update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio/',
+};
 
 export default nextConfig;
+
