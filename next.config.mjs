2c009feff2acc37f0737d48045627660e2403f1e
trantrangtech <trantrang.tech@gmail.com>
2025-01-09 17:37:39 +0700

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',  // Required for static HTML export
-    // basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
     images: {
         unoptimized: true, // Required for static export
     },
-    // assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '', 
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '', 
     publicRuntimeConfig:{
         basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
       },
